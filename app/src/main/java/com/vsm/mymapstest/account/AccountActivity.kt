8c7cf153a4be59c832fe9cb9de73bc56a4aa7b86
vsaldivarm <vsaldivarm@gmail.com>
2020-03-20 17:06:20 -0600

    implements hwid

---
+++ app/src/main/java/com/vsm/mymapstest/account/AccountActivity.kt
@@ -0,0 +1,13 @@
+package com.vsm.mymapstest.account
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vsm.mymapstest.R
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
